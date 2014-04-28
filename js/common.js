c2128e0e2513847dccfe9b45300cc3932dc90809
Jin Tian <today.xa@gmail.com>
2014-04-28 14:14:52 +0800

    开始实现预约功能，但是有个写中文内容的bug

---
+++ js/common.js
@@ -0,0 +1,32 @@
+fs = require('fs');
+
+function getYMD( aDate ){
+	var year = aDate.getFullYear();
+	var month = aDate.getMonth()+1;
+	var day = aDate.getDate();
+
+	return "" + year + "-" + month + "-" + day;
+
+}
+
+function getBookingFilename( aDate ){
+	return 'data/booking_' + getYMD(aDate) + '.json' ;
+}
+
+// 读入指定日期的预约登记表
+function getBooking( aDate ){
+	var aJson = [];
+	var strFilename = getBookingFilename(aDate) ;
+
+	if(fs.existsSync( strFilename) ){
+		var strBookingList = fs.readFileSync(strFilename);
+	    
+		console.log("getBooking(): " + strBookingList);
+
+	    aJson = JSON.parse(strBookingList);
+	}
+	
+	return aJson;
+
+	
+} 
