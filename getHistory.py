342f544138d31e913f212a98ff1c130097c1dac2
Jin Tian <today.xa@gmail.com>
2014-06-03 15:34:44 +0800

    删除空白行

---
+++ getHistory.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-  
+
+import codecs 
+import json
+import MySQLdb
+import os
+import sys
+
+      
+
+all_json = ""
+patients = sys.argv[1]
+p_list = patients.split(",")
+
+for p in p_list :
+  print p
+
+
+
+
+
+
+
+
+
+
+
