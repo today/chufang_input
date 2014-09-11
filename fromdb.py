20fcac1841030c6fe1bbc69a7d14a87134f66e16
Jin Tian <today.xa@gmail.com>
2014-09-11 15:44:03 +0800

    修改患者信息获取方式，改为从custom表中取得。

--- fromdb.py
+++ fromdb.py
@@ -6,56 +6,46 @@ import os
 import shutil
 import sys
 
+import mydb
+
 def getRecipe( p_no  ):
   bigJson = ""
   try:
-      conn=MySQLdb.connect(host='localhost',user='root',passwd='',port=3306,db='frt', charset='utf8')
+      p_json = mydb.getPatientJson( p_no )
+
+      conn = mydb.getConn()
       cur=conn.cursor()
       
-      """ 取出病人信息的Json_id  """
-      sql1 = "select json_id from t_patient where  patient_no='"+ p_no + "' order by id desc"
-      #print sql1  
-      cur.execute(sql1)
-      result=cur.fetchone()
-      if result:
-        temp_p_json_id = result[0]
+      """ 取出病历信息的 Json_id  """
+      sql2 = "select json_id from t_recipe where  patient_no='"+ p_no + "' order by id desc"
+      print sql2
+      cur.execute(sql2)
+      allResult=cur.fetchall()
+      """ 循环取出每一份病历的json串  """
+      c_json_list = []
+      for result in allResult:
+        temp_c_json_id = result[0]
+        #print temp_c_json_id
+
+        sql4 = "select json_string from t_json where  id='"+ str(temp_c_json_id) + "' "
+        cur.execute(sql4)
+        ajson = cur.fetchone()
+        c_json_list.append(ajson[0])
+        #print c_json
+
+      """ 所有的Json都取出来了，现在需要拼接字符串了。 """
+      full_p_json = '"patients":' + p_json 
+      all_c_json = '  '
+      for temp_str in c_json_list :
+        all_c_json += temp_str + ','
+      bigJson = '{ '+ full_p_json + ',' + '"cases":[' + all_c_json[0:-1] + ']' +' }'
       
-        """ 取出病人信息的Json串 """
-        sql3 = "select json_string from t_json where  id='"+ str(temp_p_json_id) + "' "
-        cur.execute(sql3)
-        result=cur.fetchone()
-        p_json = result[0]
-        #print p_json
-        
-        """ 取出病历信息的 Json_id  """
-        sql2 = "select json_id from t_recipe where  patient_no='"+ p_no + "' order by id desc"
-        #print sql2
-        cur.execute(sql2)
-        allResult=cur.fetchall()
-        """ 循环取出每一份病历的json串  """
-        c_json_list = []
-        for result in allResult:
-          temp_c_json_id = result[0]
-          #print temp_c_json_id
-
-          sql4 = "select json_string from t_json where  id='"+ str(temp_c_json_id) + "' "
-          cur.execute(sql4)
-          ajson = cur.fetchone()
-          c_json_list.append(ajson[0])
-          #print c_json
-
-        """ 所有的Json都取出来了，现在需要拼接字符串了。 """
-        full_p_json = '"patients":' + p_json 
-        all_c_json = '  '
-        for temp_str in c_json_list :
-          all_c_json += temp_str + ','
-        bigJson = '{ '+ full_p_json + ',' + '"cases":[' + all_c_json[0:-1] + ']' +' }'
-      else:
-        bigJson = "{}"  
         
       conn.commit()
       cur.close()
       conn.close()
+
+      print bigJson 
    
   except MySQLdb.Error,e:
      print "Mysql Error %d: %s" % (e.args[0], e.args[1])
@@ -66,6 +56,7 @@ def cp_img():
   for file in os.listdir('old_photos/'): 
     if not os.path.exists('../FRT_aid/img/' + file ):
       shutil.copyfile( 'old_photos/' + file , '../FRT_aid/img/' + file) 
+
 
 
 if len(sys.argv) < 2 :
